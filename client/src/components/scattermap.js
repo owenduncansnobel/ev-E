c6c60daaeb18ea106f5db4b69ffd9b3e0c434569
owen-duncan-snobel <oadster@cse.yorku.ca>
2022-01-29 11:47:25 -0500

    latitude and longitude to go to user location

--- client/src/components/scattermap.js
+++ client/src/components/scattermap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {StaticMap} from 'react-map-gl';
 import DeckGL from '@deck.gl/react';
 import {ScatterplotLayer} from '@deck.gl/layers';
@@ -11,16 +11,41 @@ const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 const DATA_URL = elec_data
 
-const INITIAL_VIEW_STATE = {
-  longitude: -79.347015,
-  latitude: 43.6532,
-  zoom: 12,
-  pitch: 10,
-  bearing: 0
-};
 
-const ScatterMap = ({data = DATA_URL, radius = 6, gasColor = GAS_COLOR, electricColor = ELECTRIC_COLOR, mapStyle = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json', height='80vh',width='80vw'}
-) => {
+
+const ScatterMap = ({
+    data = DATA_URL,
+    radius = 6, 
+    gasColor = GAS_COLOR, 
+    electricColor = ELECTRIC_COLOR, 
+    mapStyle = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json', 
+    height='80vh',
+    width='80vw'
+}) => {
+
+useEffect(() => {
+    if (!navigator.geolocation) {
+    } else {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setInitialViewState({
+                longitude: position.coords.longitude,
+                latitude: position.coords.latitude,
+                zoom: 12,
+                pitch: 10,
+                bearing: 0
+                })
+        }, () => {})
+    }
+    }, [])
+
+const [initialViewState, setInitialViewState] = useState({
+    longitude: -79.347015,
+    latitude: 43.6532,
+    zoom: 12,
+    pitch: 10,
+    bearing: 0
+    });
+
   const layers = [
     new ScatterplotLayer({
       id: 'scatter-plot',
@@ -42,7 +67,7 @@ const ScatterMap = ({data = DATA_URL, radius = 6, gasColor = GAS_COLOR, electric
   };
   
   return (
-    <DeckGL width={width} height={height} layers={layers} initialViewState={INITIAL_VIEW_STATE} controller={true} style={deckStyle}>
+    <DeckGL width={width} height={height} layers={layers} initialViewState={initialViewState} controller={true} style={deckStyle}>
       <StaticMap width={width} height={height} reuseMaps mapStyle={mapStyle} mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
     </DeckGL>
   );
--- client/src/views/home.js
+++ client/src/views/home.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ScatterMap from '../components/scattermap';
 
 const Home = () => {
@@ -6,7 +6,7 @@ const Home = () => {
     return (
         <div className='flex justify-center'>
             <div className='w-4/5 h-4/5'>
-                <ScatterMap width='80vw' height='80vh'/>
+                <ScatterMap width='80vw' height='80vh' />
             </div>
         </div>
     )
