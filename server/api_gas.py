4e50de3f1c3e91feddfb7227c892ce6876fb5893
Johan Otoka <73400373+winterx242@users.noreply.github.com>
2022-01-29 14:32:10 -0500

    created an abstraction layer in api_gas.py

--- server/api_gas.py
+++ server/api_gas.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from marshmallow_jsonapi.flask import Schema
+from marshmallow_jsonapi import fields
 
 app = Flask(__name__)
 
@@ -12,5 +14,18 @@ class Gas(db.Model):
     name = db.Column(db.String)
     latitude = db.Column(db.Integer)
     longitude = db.Column(db.Integer)
+
+# Create data abstraction layer
+class GasSchema(Schema):
+    class Meta:
+        type_ = 'gas'
+        self_view = 'gas_one'
+        self_view_kwargs = {'id': '<id>'}
+        self_view_many = 'gas_many'
+
+    id = fields.Integer()
+    name = fields.Str(required=True)
+    latitude = fields.Integer(load_only=True)
+    longitude = fields.Integer(load_only=True)
 
 db.create_all()
