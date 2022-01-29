028d75e2916f5622aed800814c1d640337e82f03
owen-duncan-snobel <oadster@cse.yorku.ca>
2022-01-29 10:53:42 -0500

    temp config and navbar

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 const Navbar = () => {
-    return (<div>
-        Navigation Bar
-    </div>)
+    return (
+    <nav className="bg-white shadow">
+        <div className="container mx-auto px-6 py-3  ">
+            <div className="flex text-center mx-auto justify-between">
+                <div className="pl-3 text-xl font-bold text-gray-500 md:text-2xl hover:text-gray-800 dark:hover:text-gray-800 ">
+                    ev-E
+                </div>
+
+                <div className="flex justify-between flex-col md:flex-row md:mx-6">
+                    <div className=" inline-flex items-center justify-between ">
+                        <div className="flex flex-col md:flex-row md:mx-6">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nav>
+    )
 }
 export default Navbar;
