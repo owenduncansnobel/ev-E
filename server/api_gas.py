cf10f882eab6b9705fdccd320b9a07b793c87ad5
Johan Otoka <73400373+winterx242@users.noreply.github.com>
2022-01-29 16:31:01 -0500

    created source managers and endpoints

--- server/api_gas.py
+++ server/api_gas.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow_jsonapi.flask import Schema
 from marshmallow_jsonapi import fields
+from flask_rest_jsonapi import Api, ResourceDetail, ResourceList
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////artists.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////gas.db'
 db = SQLAlchemy(app)
 
 # Define a class for the Gas table
@@ -14,6 +15,7 @@ class Gas(db.Model):
     name = db.Column(db.String)
     latitude = db.Column(db.Integer)
     longitude = db.Column(db.Integer)
+    type = db.Column(db.Boolean)
 
 # Create data abstraction layer
 class GasSchema(Schema):
@@ -27,5 +29,28 @@ class GasSchema(Schema):
     name = fields.Str(required=True)
     latitude = fields.Integer(load_only=True)
     longitude = fields.Integer(load_only=True)
+    type = fields.Boolean(load_only=True)
+
+# Create resource managers and endpoints
+class GasMany(ResourceList):
+    schema = GasSchema
+    data_layer = {'session': db.session,
+                  'model': Gas}
+
+
+class GasOne(ResourceDetail):
+    schema = GasSchema
+    data_layer = {'session': db.session,
+                  'model': Gas}
+
+
+api_gas = Api(app)
+api_gas.route(GasMany, 'gas_many', '/gas')
+api_gas.route(GasOne, 'gas_one', '/artist/<int:id>')
+
+
+# main loop to run app in debug mode
+if __name__ == '__main__':
+    app.run(debug=True)
 
 db.create_all()
