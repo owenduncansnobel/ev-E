8bcbff3267c030c3a6fefff34650b4dcc5f8227a
owen-duncan-snobel <oadster@cse.yorku.ca>
2022-01-29 09:42:43 -0500

    updated map and added .env

--- client/src/components/scattermap.js
+++ client/src/components/scattermap.js
@@ -1,21 +1,57 @@
-import * as React from 'react';
-import { useState } from 'react';
-import ReactMapGL from 'react-map-gl';
+import React from 'react';
+import {StaticMap} from 'react-map-gl';
+import DeckGL from '@deck.gl/react';
+import {ScatterplotLayer} from '@deck.gl/layers';
+import elec_data from './geotab.json';
 
-function Map() {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
-  });
+const GAS_COLOR = [0, 128, 255];
+const ELECTRIC_COLOR = [255, 0, 128];
+
+const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
+
+
+
+// Source data CSV
+
+
+const DATA_URL = elec_data
+
+
+const INITIAL_VIEW_STATE = {
+  longitude: -79.347015,
+  latitude: 43.6532,
+  zoom: 12,
+  pitch: 10,
+  bearing: 0
+};
+
+export default function App({
+  data = DATA_URL,
+  radius = 6,
+  gasColor = GAS_COLOR,
+  electricColor = ELECTRIC_COLOR,
+  mapStyle = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json'
+}) {
+  const layers = [
+    new ScatterplotLayer({
+      id: 'scatter-plot',
+      data,
+      radiusScale: radius,
+      radiusMinPixels: 1.2,
+      getPosition: d => [d[0], d[1]],
+      getFillColor: d => d[2] === 0 ? gasColor : electricColor,
+      getRadius: 2,
+      cluster: true,
+      updateTriggers: {
+        getFillColor: [gasColor, electricColor]
+      }
+    })
+  ];
 
   return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={nextViewport => setViewport(nextViewport)}
-    />
+    <DeckGL layers={layers} initialViewState={INITIAL_VIEW_STATE} controller={true}>
+        {console.log(MAPBOX_ACCESS_TOKEN)}
+      <StaticMap reuseMaps mapStyle={mapStyle} preventStyleDiffing={true} mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+    </DeckGL>
   );
 }
-export default Map;
