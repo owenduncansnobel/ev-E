2ded21e1bfd681a292a1e7e7b98f423d97307b89
Fayaz Rafin <67512015+fayaz-rafin@users.noreply.github.com>
2022-01-29 15:14:46 -0500

    Added title text and a search bar

---
+++ client/src/components/search.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+function searchBar () {
+    return (
+    <div className='search-bar'>
+        <input type="text" placeholder="Search"/>
+    </div>
+    )
+}
+
+export default searchBar;
