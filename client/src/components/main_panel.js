074617323499a0290a5b4b96f0a8781684aa1d06
owen-duncan-snobel <oadster@cse.yorku.ca>
2022-01-29 16:08:05 -0500

    rearranged the panels

--- client/src/components/main_panel.js
+++ client/src/components/main_panel.js
@@ -3,7 +3,12 @@ import chat from '../images/locker-dynamic-clay.png';
 
 const MainPanel = () => {
     return (
+<>
     <div className="grid p-10 pb-20 lg:grid-cols-2">
+        <div className="">
+            <img className="object-cover" src={chat} />
+        </div>
+        
         <div className="mx-auto pt-40 pl-20">
             <div className='text-right'>
                 <div className='inline-block text-left'>
@@ -13,10 +18,23 @@ const MainPanel = () => {
                 </div>
             </div>
         </div>
+    </div>
+
+    <div className="grid p-10 pb-20 lg:grid-cols-2"> 
+
+    <div className="mx-auto pt-40 pl-20">
+            <div className='text-right'>
+                <div className='inline-block text-left'>
+                    <h1 className='text-4xl font-bold text-gray-100'>Currently, the number of <span className='text-blue-300'>Gas</span> stations far out numbers the <span className='text-green-300'>Electric charging</span> stations</h1>
+                </div>
+            </div>
+        </div>
+
         <div className="">
-            <img className="object-cover" src={chat} />
+            
         </div>
     </div>
+</>
     )
 }
 export default MainPanel;
