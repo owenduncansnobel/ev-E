38f74553cda7c7a0d158ee32357bbafbee2337a2
Fayaz Rafin <67512015+fayaz-rafin@users.noreply.github.com>
2022-01-29 15:46:05 -0500

    Modified the title component

--- client/src/views/home.js
+++ client/src/views/home.js
@@ -7,11 +7,7 @@ const Home = () => {
     return (
         <div className='flex justify-center mb-5'>
             <div className='w-4/5 h-4/5 rounded-full pt-10'>
-            <div className="text-center font-bold text-gray-800 md:text-2xl hover:text-gray-800 dark:hover:text-gray-800 drop-shadow-2xl">
-                    <span className='text-green-300 text-7xl'>e</span> 
-                    <span className='text-blue-300 text-8xl'>V</span>
-                    <span className='text-green-300 text-7xl'>e</span>
-                </div>
+                
 
                 <MainPanel />
                 <ScatterMap width='80vw' height='80vh' />
