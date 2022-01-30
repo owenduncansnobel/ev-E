cd9575e4f971b3375b75b18536d6ed42ec33fe85
palvisha13 <palvisha@my.yorku.ca>
2022-01-29 21:57:55 -0500

    setting up googleapi

--- server/api.py
+++ server/api.py
@@ -1,13 +1,25 @@
 from flask import Flask, jsonify
 import json
 
+
+# create the flask app
 app = Flask(__name__)
 @app.route("/api", methods=['GET'])
 
 
-#read in the json file data through this function 
+# main functions 
 
 def api():
+
+    """ 
+    This function defines the flask app used 
+    to test this code. 
+    -------
+    Input: None
+    Output: specific data from json file extracted,
+    then jsonifyed and returned again
+    
+    """
     result = []
     for i in evdata["fuel_stations"]:
         result.append({"id": i["id"],
@@ -17,20 +29,31 @@ def api():
        "type:": 0 })
     return jsonify(result), 200
 
-
-key_list = ["id", "city", "long", "lat"]
-
 def jsonfiles(filename):
+
+    """
+    This function reads in a json file and returns stores it in a variable 
+    that is returned 
+    ------
+    Input: json file name
+    Output: variable storing json data
+    """
+
     with open(filename) as f:
         data = json.load(f)
     return data
 
-# data that will be displayed 
-
+# read in json data using function above
 evdata = jsonfiles('electricity.json')
 
+
+with open("apikey.txt") as f: 
+    api_key = f.readline()
+    f.close
 
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
